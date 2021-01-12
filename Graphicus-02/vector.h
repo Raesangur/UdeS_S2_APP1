02cc09a139f6033942bd33bfb915ef073a677322
Raesangur <pascalemmanuel.lachance@gmail.com>
2021-01-12 16:07:53 -0500

    vector declaration

---
+++ Graphicus-02/vector.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <cstddef>
+
+
+template<typename ItemType>
+class vector
+{
+private:
+	using Iterator = ItemType*;
+
+	Iterator m_begin = nullptr;
+	Iterator m_end = nullptr;
+	size_t   m_capacity = 0;
+
+	void m_reallocate(size_t newCapacity);
+
+public:
+	vector(size_t count);
+	vector(size_t count, const ItemType& value);
+	vector(const vector& other);
+	~vector();
+
+	ItemType& operator[](size_t index);
+	Iterator begin();
+	Iterator end();
+
+	size_t size();
+	size_t capacity();
+	void reserve(size_t capacity);
+	void shrink_to_fit();
+
+	void clear();
+	void push_back(const ItemType& value, size_t count = 1);
+	void pop_back(size_t count = 1);
+};
+
