ebfa0d7c6e5920a0ea049c763b7d794fc3e520b7
Raesangur <pascalemmanuel.lachance@gmail.com>
2021-01-12 10:33:30 -0500

    init Graphicus-02

---
+++ Graphicus-02/forme.h
@@ -0,0 +1,30 @@
+/********
+ * Fichier: forme.h
+ * Auteurs: C.-A. Brunet
+ * Date: 08 janvier 2018 (creation)
+ * Description: Declaration des classes de formes geometriques. La
+ *    classe Forme ne doit pas etre modifiee. Ce fichier fait partie de
+ *    la distribution de Graphicus.
+********/
+
+#ifndef FORME_H
+#define FORME_H
+
+#include <iostream>
+#include "coordonnee.h"
+
+class Forme
+{
+public:
+  Forme(int x = 0, int y = 0);
+  virtual ~Forme();
+  void translater(int deltaX, int deltaY);
+  Coordonnee getAncrage();
+  void setAncrage(Coordonnee c);
+  virtual double aire() = 0;
+  virtual void afficher(std::ostream& s) = 0;
+protected:
+	Coordonnee ancrage;
+};
+
+#endif
---
+++ Graphicus-02/graphicus-01.cpp
@@ -0,0 +1,17 @@
+/********
+ * Fichier: graphicus-01.cpp
+ * Auteurs: C.-A. Brunet
+ * Date: 08 janvier 2018 (creation)
+ * Description: gestionnaire de tests pour l'application Graphicus. Ce
+ *    fichier fait partie de la distribution de Graphicus.
+********/
+#include "tests.h"
+
+int main()
+{
+   Tests tests;
+	   
+   tests.tests_application();
+   return 0;
+}
+
