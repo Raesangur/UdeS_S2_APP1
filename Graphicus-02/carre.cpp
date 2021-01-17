f43864a6a3dc7cdb9ab251f6528ac4e34e2628a9
rivo3201 <rivo3201@usherbrooke.ca>
2021-01-17 17:52:56 -0500

    ajout rectangleETcarre

--- Graphicus-02/carre.cpp
+++ Graphicus-02/carre.cpp
@@ -1,1 +1,17 @@
 //carre.cpp
+#include <cstddef>
+#include "forme.h"
+#include "rectangle.h"
+#include "carre.h"
+
+Carre::Carre()
+{
+   setCote(1);
+   setAncrage{0, 0};
+}
+
+Carre::Carre(size_t cote, Coordonnee ptAncrage)
+{
+   setCote(cote);
+   setAncrage(ptAncrage);
+}
--- Graphicus-02/rectangle.cpp
+++ Graphicus-02/rectangle.cpp
@@ -1,1 +1,20 @@
 //rectangle.cpp
+#include <cstddef>
+#include "forme.h"
+#include "rectangle.h"
+
+
+//Initialisation H, L, ptAncrage lors de l'appel du constructeur
+Rectangle::Rectangle()
+{
+   setLargeur(1);
+   setHauteur(1);
+   setAncrage({0, 0});
+}
+
+Rectangle::Rectangle(size_t L, size_t H, Coordonnee ptAncrage)
+{
+   setLargeur(L);
+   setHauteur(H);
+   setAncrage(ptAncrage);
+}
